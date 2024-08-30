71dce199ebf86e3ea14dcf4ad5be7f967e709597
pantaelaman <calvin@theophoffs.com>
2024-08-30 11:52:59 -0400

    First booting commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+#![no_std]
+#![no_main]
+
+static WELCOME_TEXT: &[u8] = b"Welcome to TITANIUM";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in WELCOME_TEXT.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
